1eeb7c54f425cf86b8e740ec207435e8d54e2986
DEBROOP GHOSH <debroopghosh2012@gmail.com>
2023-09-09 01:26:43 +0530

    Update: Fixed Responsiveness

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
-function addTask(){
-    if(inputBox.value===''){
+function addTask() {
+    if (inputBox.value === '') {
         alert("You should write something!");
- 
+
     }
-    else{
+    else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
@@ -15,24 +15,24 @@ function addTask(){
         li.appendChild(span);
 
 
-    } 
+    }
     inputBox.value = "";
     saveData();
 }
-listContainer.addEventListener("click",function(e){
-    if(e.target.tagName ==="LI"){
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
         saveData();
     }
-    else if(e.target.tagName ==="SPAN"){
+    else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
-},false);
-function saveData(){
-    localStorage.setItem("data",listContainer.innerHTML);
+}, false);
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
 }
-function showTask(){
-    listContainer.innerHTML= localStorage.getItem("data");
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask();
